f3701e3dd9e8631d6a9aaf362fe090eba9b894ee
Natan Moraes <nfms.15@icloud.com>
2024-05-09 15:26:59 -0300

    Criação do objeto que recebe os atributos do bd
    Criação do objeto "conexao", que recebe o metódo MySqlConnection, e os dados do bd do localhost,

--- GestorDeEstudantes_Natan/MeuBancoDeDados.cs
+++ GestorDeEstudantes_Natan/MeuBancoDeDados.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace GestorDeEstudantes_Natan
 {
     internal class MeuBancoDeDados
     {
+        //Criação de um objeto "conexao", do tipo "MySqlConnection", na classe "MeuBancoDeDados"
+        //que recebe os dados do banco de dados (localização nas aspas)
+        private MySqlConnection conexao = new MySqlConnection("datasource=localhost;port=3306;username=root;password=;database=sga_estudantes_bd_t6");
     }
 }
+
