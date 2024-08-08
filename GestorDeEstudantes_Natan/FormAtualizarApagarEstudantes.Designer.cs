4fc4fd4a33f7f58670f855311d5ac0923ebde6a7
Natan Moraes <nfms.15@icloud.com>
2024-08-08 15:28:13 -0300

    Verificar
    Atualização no método "Verificar" e acrescentar o ID como um dos critérios de atualização de alunos.

--- GestorDeEstudantes_Natan/FormAtualizarApagarEstudantes.Designer.cs
+++ GestorDeEstudantes_Natan/FormAtualizarApagarEstudantes.Designer.cs
@@ -89,6 +89,7 @@
             this.buttonSalvar.TabIndex = 19;
             this.buttonSalvar.Text = "Salvar";
             this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
             // 
             // pictureBoxUsuario
             // 
--- GestorDeEstudantes_Natan/FormAtualizarApagarEstudantes.cs
+++ GestorDeEstudantes_Natan/FormAtualizarApagarEstudantes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -35,5 +36,76 @@ namespace GestorDeEstudantes_Natan
         {
 
         }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            //Cria um estudante.
+            Estudante estudante = new Estudante();
+            //Variáveis auxiliares
+            int id = Convert.ToInt32(textBoxID.Text);
+            string nome = textBoxNome.Text;
+            string sobrenome = textBoxSobrenome.Text;
+            DateTime nascimento = dateTimePickerNascimento.Value;
+            string telefone = textBoxTelefone.Text;
+            string endereco = textBoxEndereco.Text;
+            string genero = "Feminino";
+
+            //verifica se outro genero está selecionado
+            if (radioButtonMasc.Checked)
+            {
+                genero = "Masculino";
+            }
+
+            MemoryStream foto = new MemoryStream();
+
+            //Precisamos verificar se o estudante tem mais de 10 anos e menos de 100
+
+            int anoDeNascimento = dateTimePickerNascimento.Value.Year;
+            int anoAtual = DateTime.Now.Year;
+
+            if (((anoAtual - anoDeNascimento) < 10) ||
+                ((anoAtual - anoDeNascimento) > 100))
+            {
+                MessageBox.Show("Precisa ter entre 10 e 100 anos.",
+                    "Idade inválida", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (Verificar())
+            {
+                pictureBoxUsuario.Image.Save(foto, pictureBoxUsuario.Image.RawFormat);
+
+                if (estudante.inserirEstudante(nome, sobrenome, nascimento, telefone, genero, endereco, foto))
+                {
+                    MessageBox.Show("Novo aluno cadastro!", "Sucesso!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Aluno não cadastrado!", "Falha!",
+                       MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Campos não preenchidos!", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            bool Verificar()
+            {
+                if ((textBoxNome.Text.Trim() == "") ||
+                    (textBoxSobrenome.Text.Trim() == "") ||
+                    (textBoxTelefone.Text.Trim() == "") ||
+                    (textBoxEndereco.Text.Trim() == "") ||
+                    (pictureBoxUsuario.Image == null))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+
+
+            }
+        }
     }
-}
+} 
